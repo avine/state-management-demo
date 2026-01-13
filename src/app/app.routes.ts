1a3f46590a9fea19de588f0e66d39b9faa905538
Stéphane Francel <stephane.francel@gmail.com>
2026-01-13 14:25:02 +0100

    Add scaffolding (wip)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import MaterialDemo from './material-demo/material-demo';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    component: Home,
+  },
+  {
+    path: 'material',
     component: MaterialDemo,
   },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,32 @@
 import { Component, signal, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   host: { class: 'app-root' },
-  imports: [RouterOutlet],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class App {
-  protected readonly title = signal('state-management-demo');
+  protected sidenavOpened = signal(true);
+
+  protected toggleSidenav() {
+    this.sidenavOpened.update((opened) => !opened);
+  }
 }
